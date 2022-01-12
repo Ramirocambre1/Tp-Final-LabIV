7847b0a60b1e93332fc6711f408105b3168ce90d
Ramirocambre1 <39927232+Ramirocambre1@users.noreply.github.com>
2022-01-12 09:49:53 -0300

    Delete Tp-Final directory

--- Tp-Final/Views/validate-session-company.php
+++
@@ -1,7 +0,0 @@
-<?php
-
- 
-  if(!isset($_SESSION["loggedCompany"]))
-    header("location:../index.php");  
-    
-?>
--- Tp-Final/Views/validate-session.php
+++
@@ -1,7 +0,0 @@
-<?php
-
- 
-  if(!isset($_SESSION["loggedStudent"]))
-    header("location:../index.php");  
-    
-?>
